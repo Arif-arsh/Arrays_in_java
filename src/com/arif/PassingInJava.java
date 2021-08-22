cff1b3c20cf227a67490447c3bab0acbd792d6ea
Arif-arsh <akhan7371@gmail.com>
2021-08-22 09:41:46 +0530

    passing function with array

---
+++ src/com/arif/PassingInJava.java
@@ -0,0 +1,17 @@
+package com.arif;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+
+public class PassingInJava {
+
+    public static void main(String[] args) {
+        int [] nums = {1,2,4,5};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    static void change(int[] arr) {
+        arr[0] = 99;
+    }
+}
