1ca75888bc199f8ed7cc73af51a25014c593b4dd
Arif-arsh <akhan7371@gmail.com>
2021-08-25 12:54:14 +0530

    find maximum value in a given array

---
+++ src/com/arif/Max.java
@@ -0,0 +1,29 @@
+package com.arif;
+
+public class Max {
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 2, 7, 1};
+        System.out.println(maxRange(arr, 0, 3));
+    }
+
+    static int maxRange(int[] arr, int start, int end) {
+        int maxValue = arr[start];
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+        }
+        return maxValue;
+    }
+
+
+//    static int max(int[] arr) {
+//        int maxValue = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i] > maxValue) {
+//                maxValue = arr[i];
+//            }
+//        }
+//        return maxValue;
+//    }
+}
